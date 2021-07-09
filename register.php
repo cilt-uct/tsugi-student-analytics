1b84b42fea44f8b9b5972eb36c2b57c31264c2fe
TurRil <lightscape2001@gmail.com>
2021-07-09 14:37:14 +0200

    TSUG-44: fixed description in register.php

--- register.php
+++ register.php
@@ -4,12 +4,15 @@ $REGISTER_LTI2 = array(
     "name" => "Student Analytics"
     ,"FontAwesome" => "fa-chart-bar"
     ,"short_name" => "Student Analytics"
-    ,"description" => "Shows the Student Analytics"
+    ,"description" => "Shows a breakdown of student activity over the weeks that the course is active, needs access to a Learning Warehouse that stores the student activity classification."
     ,"messages" => array("launch") // By default, accept launch messages..
     ,"privacy_level" => "public" // anonymous, name_only, public
     ,"license" => "Apache"
     ,"languages" => array(
         "English",
+    )
+    ,"analytics" => array(
+        "internal"
     )
     ,"source_url" => "https://github.com/cilt-uct/tsugi-student-analytics"
     // For now Tsugi tools delegate this to /lti/store
